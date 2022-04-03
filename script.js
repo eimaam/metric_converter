81dd1b690913334aed5b461149a480091e4df4a4
eimaam <imamddahir@gmail.com>
2022-04-03 12:36:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function solve(){
+    let main = document.getElementById("primary").value;
+    let val = main/100;
+    let conv = document.getElementById("converted");
+    conv.innerHTML = val;
+}
