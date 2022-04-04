5c4a96354f607d46800e6bbafcd543c2c9367e6b
eimaam <imamddahir@gmail.com>
2022-04-04 12:17:18 +0100

    main value display to change ..scripted

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+var myButton = document.getElementById("btn");
+myButton.addEventListener("click", solve);
+
 
 function solve(){
-    let main = document.getElementById("primary").value;
-    let val = main/100;
-    let conv = document.getElementById("converted");
-    conv.innerHTML = val;
+    // Kilometer to Meter 
+    var main = document.getElementById("primary").value;
+    var mainVal = document.querySelectorAll(".val");
+    let mainValArr = Array.from(mainVal);
+    for (let i=0; i < mainValArr.length; i++){
+        mainValArr[i].innerHTML = main;
+    }
+    // mainValArr.forEach()
+    
+    // let mainValArr = Array.from(mainVal);
+    // mainValArr.forEach(mainValArr => {mainValArr.innerHTML = main})
+    
+    
+
 }
+//     // LENGHT:
+//     // cm to m
+
+//     function solveCM(){
+//         var cm = document.getElementById("val2");
+//         cm.innerHTML = main;
+//         var m = document.getElementById("res2");
+//         result = math.floor(math.main/100;
+//         m.innerHTML = result;
+//     }
