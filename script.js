94425377c6b95b70e26d8525e37be22b407b65cd
eimaam <imamddahir@gmail.com>
2022-04-05 12:00:00 +0100

    conversions fully passed

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 var myButton = document.getElementById("btn");
-myButton.addEventListener("click", solve);
+myButton.addEventListener("click", displayValue);
+myButton.addEventListener("click", convert);
 
+var main = document.getElementById("primary");
 
-function solve(){
-    // Kilometer to Meter 
-    var main = document.getElementById("primary").value;
+// function to display value entered
+function displayValue(){
+     
     var mainVal = document.querySelectorAll(".val");
     let mainValArr = Array.from(mainVal);
     for (let i=0; i < mainValArr.length; i++){
-        mainValArr[i].innerHTML = main;
+        mainValArr[i].innerHTML = main.value;
     }
-    // mainValArr.forEach()
-    
-    // let mainValArr = Array.from(mainVal);
-    // mainValArr.forEach(mainValArr => {mainValArr.innerHTML = main})
-    
-    
-
 }
-//     // LENGHT:
-//     // cm to m
 
-//     function solveCM(){
-//         var cm = document.getElementById("val2");
-//         cm.innerHTML = main;
-//         var m = document.getElementById("res2");
-//         result = math.floor(math.main/100;
-//         m.innerHTML = result;
-//     }
+function convert(){
+    // Variables of each result
+    let km2m = document.getElementById("res1");
+    let cm2m = document.getElementById("res2");
+    let f2in = document.getElementById("res3");
+    let C2K = document.getElementById("res4");
+    let F2C = document.getElementById("res5");
+    let L2mL = document.getElementById("res6");
+    let lb2kg = document.getElementById("res7");
+    let J2ergs = document.getElementById("res8");
+    let sqft2sqm = document.getElementById("res9");
+    let Mach2Knot = document.getElementById("res10");
+
+    // function that performs the conversion
+    function con(){
+        // taking values using empty array and using the push method to add values to the array
+        // And returns res which marks a stop to the function
+        var res =[]
+        res.push(main.value*1000);
+        res.push(main.value/100);
+        res.push(main.value*12);
+        res.push(main.value*(1)+273.15);
+        res.push((main.value-32)*(5/9));
+        res.push(main.value*1000);
+        res.push(main.value*0.454);
+        res.push(main.value*10000000);
+        res.push(main.value*0.092903);
+        res.push(main.value*666.739);
+        return res;
+    }
+    // calling on the array and assigning/displaying it to respective push values declared above using DOM
+    var result = con() 
+    km2m.innerHTML = result[0] 
+    cm2m.innerHTML = result[1] 
+    f2in.innerHTML = result[2] 
+    C2K.innerHTML = result[3] 
+    F2C.innerHTML = result[4] 
+    L2mL.innerHTML = result[5] 
+    lb2kg.innerHTML = result[6] 
+    J2ergs.innerHTML = result[7] 
+    sqft2sqm.innerHTML = result[8] 
+    Mach2Knot.innerHTML = result[9] 
+}
